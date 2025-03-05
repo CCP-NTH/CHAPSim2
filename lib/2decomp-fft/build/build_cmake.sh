dd4b9c105c6e806312bec0937113487e60164640
Wei Wang <72932587+weiwangstfc@users.noreply.github.com>
2025-03-05 19:36:20 +0000

    removed build folder in 2decomp lib

--- lib/2decomp-fft/build/build_cmake.sh
+++ lib/2decomp-fft/build/build_cmake.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 FC=mpif90 
+
+find . -mindepth 1 ! -name "build_cmake.sh" -delete
+
+#cmake . -DCMAKE_PREFIX_PATH=/usr/bin/mpif90
+#cmake -DCMAKE_FORTRAN_COMPILER=/usr/bin/mpif90 .
 cmake -S ../ -B ./
 cmake --build ./
 cmake --install ./
--- lib/2decomp-fft/build/decomp2d-config.cmake
+++
@@ -1,3 +0,0 @@
-include("/Users/wei.wang/Work/11_CHAPSim/CHAPSim2/lib/2decomp-fft/build/decomp2d-targets.cmake")
-
-set(decomp2d_INCLUDE_DIRS "/Users/wei.wang/Work/11_CHAPSim/CHAPSim2/lib/2decomp-fft/build/include")
