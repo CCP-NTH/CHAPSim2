40e3c74d1d20a6e62e96937a64aeee1be26e9b65
Wei Wang <72932587+weiwangstfc@users.noreply.github.com>
2025-07-04 10:56:35 +0100

    updated scripts

--- examples/3_monitor/plot_monitor_points.py
+++ examples/3_monitor/plot_monitor_points.py
@@ -74,7 +74,7 @@ def plot_monitor_points(N, M):
         successful_files.append(filename)
         print(f"Successfully reading data from: {filename}")
         
-        # Skip every 100 points
+        # Skip every M points
         time = data[::M, 0]
         
         # Plot each variable
