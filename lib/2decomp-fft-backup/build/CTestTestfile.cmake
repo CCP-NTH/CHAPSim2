c82a28bbf4460f9adf738f941de1796f78ea0f5c
Wei Wang <wei.wang@stfc.ac.uk>
2025-12-20 21:50:17 +0000

    deleted unused old version of 2decomp lib

--- lib/2decomp-fft-backup/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/weiwang/Work_RSDevelopment/1_CHAPSim/CHAPSim2/lib/2decomp-fft-backup
-# Build directory: /home/weiwang/Work_RSDevelopment/1_CHAPSim/CHAPSim2/lib/2decomp-fft-backup/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
